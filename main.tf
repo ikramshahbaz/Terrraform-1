570127f21efa878c48f59e6f525ab944e8973380
ikramshahbaz <ikramshahbaz@gmail.com>
2021-08-13 11:25:52 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,92 +1,125 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "resourcegroup" {
   name     = var.rgname
   location = var.location
-  tags = var.tags
+  tags = {
+    name = "shahbaz"
+  }
 
   provisioner "local-exec" {
     command = "echo '${self.id}' "
   }
 }
 
-resource "azurerm_virtual_network" "main_network" {
-  name                           = var.main_network_name
-  resource_group_name            = var.rgname
-  location                       = var.location
-  address_space                  = var.main_address_space
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "key-vault" {
+  depends_on                  = [azurerm_resource_group.resourcegroup]
+  name                        = "shahbazikram"
+  location                    = var.location
+  resource_group_name         = var.rgname
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+  }
 }
 
-resource "azurerm_subnet" "internal_subnet" {
-#  count                         = length(var.internal_subnet_address_space)
-  count                  = var.internal_subnet_address_count
-  name                   = "${var.subnet_name}-${count.index}"
-  resource_group_name    = var.rgname
-  virtual_network_name   = azurerm_virtual_network.main_network.name
-  address_prefixes       = [element(var.internal_subnet_address_space, count.index)]
+
+resource "azurerm_key_vault_secret" "vm_password" {
+  name         = "linux-vm-password"
+  value        = "Azure@1234"
+  key_vault_id = azurerm_key_vault.key-vault.id
+  depends_on   = [azurerm_key_vault.key-vault]
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "network-metis"
+  resource_group_name = var.rgname
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "testsubnet"
+  resource_group_name  = var.rgname
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "security_group" {
-
-  name                = var.security_group_name
+  name                = "acceptanceTestSecurityGroup1"
   location            = var.location
   resource_group_name = var.rgname
 
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-  dynamic "security_rule" {
-    for_each = var.inbound_port_ranges
-    content {
-      name                       = "rule-${security_rule.value}"
-      priority                   = security_rule.key
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = security_rule.value
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
+  tags = {
+    environment = "Production"
   }
 }
-#resource "azurerm_network_security_rule" "security_group_rule" {
-#  count                       = length(var.inbound_port_ranges)
-#  name                        = "rule-${element(var.inbound_port_ranges, count.index)}"
-#  priority                    = "${(100 * (count.index + 1))}"
-#  direction                   = "Inbound"
-#  access                      = "Allow"
-#  protocol                    = "Tcp"
-#  source_port_range           = "*"
-#  destination_port_range      = "*"
-#  source_address_prefix       = "*"
-#  destination_address_prefix  = element(var.inbound_port_ranges, count.index)
-#  resource_group_name         = var.rgname
-#  network_security_group_name = var.security_group_name
-#}
+
 
 resource "azurerm_subnet_network_security_group_association" "security_group_associate" {
-  count 					= var.internal_subnet_address_count
-  subnet_id                 = element(azurerm_subnet.internal_subnet.*.id, count.index)
+  subnet_id                 = azurerm_subnet.internal.id
   network_security_group_id = azurerm_network_security_group.security_group.id
 }
 
-resource "azurerm_network_interface" "main_interface" {
-  count 			  = var.internal_subnet_address_count
-  name                = "vm-int-${count.index}"
+resource "azurerm_public_ip" "public_ip" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = var.rgname
+  location            = var.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface" "main1" {
+  name                = "vm-01-nic"
   location            = var.location
   resource_group_name = var.rgname
 
   ip_configuration {
-    name                          = "ip-vm-${count.index}"
+    name                          = "ip-vm-01"
     private_ip_address_allocation = "Dynamic"
-    #public_ip_address_id          = azurerm_public_ip.public_ip.id
-    subnet_id                     = element(azurerm_subnet.internal_subnet.*.id, count.index)
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    subnet_id                     = azurerm_subnet.internal.id
   }
 }
 
-resource "azurerm_virtual_machine" "vm_main" {
-  count 						   = length(azurerm_network_interface.main_interface.*.id)
-  name                             = "vm-${count.index}"
+
+
+resource "azurerm_virtual_machine" "main2" {
+  name                             = "vm-01"
   location                         = var.location
   resource_group_name              = var.rgname
-  network_interface_ids            = [element(azurerm_network_interface.main_interface.*.id, count.index)]
+  network_interface_ids            = [azurerm_network_interface.main1.id]
   vm_size                          = "Standard_DS1_v2"
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
@@ -99,7 +132,7 @@ resource "azurerm_virtual_machine" "vm_main" {
   }
 
   storage_os_disk {
-    name              = "vm-osdisk-${count.index}"
+    name              = "vm-01-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     disk_size_gb      = 30
@@ -107,11 +140,12 @@ resource "azurerm_virtual_machine" "vm_main" {
   }
 
   os_profile {
-    computer_name  = "vm-${count.index}"
+    computer_name  = "vm-01"
     admin_username = "testadmin"
-    admin_password = "Password1234"
+    admin_password = azurerm_key_vault_secret.vm_password.value
   }
-   os_profile_linux_config {
+
+  os_profile_linux_config {
     disable_password_authentication = false
   }
 }
--- variable.tf
+++ variable.tf
@@ -1,40 +1,9 @@
-variable "rgname"  {
-        type = string
+variable "rgname" {
+  type    = string
+  default = "terraform"
 }
 
 variable "location" {
-        type = string
-}
-
-
-variable "main_network_name" {
-        type = string
-}
-
-variable "main_address_space" {
-        type = list(string)
-}
-
-variable "tags" {
-        type  = map(string)
-}
-
-variable "subnet_name" {
-        type = string
-}
-
-variable "internal_subnet_address_count" {
-        type = number
-}
-
-variable "internal_subnet_address_space" {
-        type = list(string)
-}
-
-variable "security_group_name" {
   type    = string
-}
-
-variable "inbound_port_ranges" {
-  type = any
+  default = "east us"
 }
