78277c1d934a43e0d14d65467e2e36610f11d95a
ikramshahbaz <ikramshahbaz@gmail.com>
2021-08-13 12:10:48 +0530

    first commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "resourcegroup" {
   name     = var.rgname
   location = var.location
   tags = {
-    name = "ikram"
+    name = "shahbaz"
   }
 
   provisioner "local-exec" {
